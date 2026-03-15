7358f833d6559b2fe0582483abd898a7b6027cfb
muhaimanalishah <miloasdev@gmail.com>
2026-03-16 00:47:24 +0500

    feat: Remove Tailwind CSS integration and update responsive styles for BlogPost and About pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muhaiman.dev',
   integrations: [mdx(), sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
