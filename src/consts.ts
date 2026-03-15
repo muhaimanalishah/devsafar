fc2fe9868dad825aa85c0516457cb4a9dd217e6e
muhaimanalishah <miloasdev@gmail.com>
2026-03-16 00:55:40 +0500

    format: format the whole codebase

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,12 @@
 export const SITE_TITLE = 'devsafar';
-export const SITE_DESCRIPTION = 'documenting the safar of a pakistani dev. bridging the gap for newcomers through public logs and broken configs.';
+export const SITE_DESCRIPTION =
+  'documenting the safar of a pakistani dev. bridging the gap for newcomers through public logs and broken configs.';
 
 export const AUTHOR_NAME_FULL = 'muhaiman ali shah';
 export const AUTHOR_NAME = 'muhaiman';
 export const AUTHOR_EMAIL = 'miloasdev@gmail.com';
 
 export const SOCIALS = {
-github: 'https://github.com/muhaimanalishah',
-x: 'https://x.com/muhaiman_as',
+  github: 'https://github.com/muhaimanalishah',
+  x: 'https://x.com/muhaiman_as',
 };
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,17 +3,17 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			tag: z.enum(['tutorial', 'article']).optional(),
-		}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      tag: z.enum(['tutorial', 'article']).optional(),
+    }),
 });
 
 export const collections = { blog };
